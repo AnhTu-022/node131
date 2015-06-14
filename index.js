73a593b837cbed50bd06d87d758e580092dfaa94
anhtu-022 <anhtu.131@gmail.com>
2015-06-14 22:26:16 +0200

    asf

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 var express = require('express');
-var app = express(),
-	http = require('http'),
-	io = require('socket.io').listen(server, {log: false});
-
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-
-/*app.get('/', function(request, response) {
-  response.send('Hello World!');
-});*/
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/view.html');
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function(s) {
   console.log('Node app is running on port', app.get('port'));
+ 
 });
-
