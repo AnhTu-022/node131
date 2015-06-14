b8967d2cb0b2c7f5647aa7a6914c25b0f06d4791
anhtu-022 <anhtu.131@gmail.com>
2015-06-14 21:50:06 +0200

    a

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -14,4 +16,26 @@ app.get('/', function(req, res){
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+//log connection
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+io.emit('some event', { for: 'everyone' });
+io.on('connection', function(socket){
+  socket.broadcast.emit('hi');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
