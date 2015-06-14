996c0e03727687878b0e151d1f0397f6a1ba44f2
anhtu-022 <anhtu.131@gmail.com>
2015-06-14 22:52:26 +0200

    worked

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+/*var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');*/
+
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -8,7 +15,27 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/view.html');
 });
 
-app.listen(app.get('port'), function(s) {
+/*app.listen(app.get('port'), function(s) {
   console.log('Node app is running on port', app.get('port'));
- 
+});*/
+
+http.listen(app.get('port'), function(){
+  console.log('listening on :', app.get('port'));
+});
+
+//log connection
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
